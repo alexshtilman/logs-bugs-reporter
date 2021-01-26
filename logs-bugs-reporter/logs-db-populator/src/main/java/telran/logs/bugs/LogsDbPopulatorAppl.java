7809f4e4b80a5a2e602ed347095c3c410537bf11
Alex Shtilman <gungam@outlook.com>
2021-01-26 23:15:31 +0200

    home work 56 sneaky update 2

--- logs-bugs-reporter/logs-db-populator/src/main/java/telran/logs/bugs/LogsDbPopulatorAppl.java
+++ logs-bugs-reporter/logs-db-populator/src/main/java/telran/logs/bugs/LogsDbPopulatorAppl.java
@@ -31,7 +31,7 @@ public class LogsDbPopulatorAppl {
 	try {
 	    consumerLogs.save(new LogDoc(logDto));
 	} catch (Exception e) {
-	    System.out.println(e.getMessage());
+	    System.out.println(logDto + " cased by: " + e.getMessage());
 	}
     }
 }
